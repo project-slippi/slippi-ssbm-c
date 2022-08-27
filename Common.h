047f282bf752a57cb222475f871d0189b9ccae78
Robert Peralta <rapito@gmail.com>
2022-08-27 02:49:14 -0400

    Stop OSReporting hack to reset floating pointers, fix to createSubtext was the solution + Added missing extra functions on Common.h

--- Common.h
+++ Common.h
@@ -9,10 +9,37 @@
 /** Functions **/
 void* (*bp)(void) = (void *)0x8021b2d8;
 
+/** HSD Functions **/
+
 bool GOBJ_IsAlive(GOBJ* gobj)
 {
     return gobj != NULL;
 }
+
+void JOBJ_SetAllAlpha(JOBJ *jobj, float alpha) {
+
+    if (!jobj) return;
+
+    DOBJ *dobj = jobj->dobj;
+    while (dobj) {
+        if (dobj->mobj && dobj->mobj->mat)
+            dobj->mobj->mat->alpha = alpha;
+        dobj = dobj->next;
+    }
+
+    JOBJ_SetAllAlpha(jobj->child, alpha);
+    JOBJ_SetAllAlpha(jobj->sibling, alpha);
+}
+
+void JOBJ_SetMatFrame(JOBJ* jobj, float frame) {
+JOBJ_ForEachAnim(jobj, 0x6, 0x400, AOBJ_ReqAnim, 1, frame);
+JOBJ_AnimAll(jobj);
+JOBJ_ForEachAnim(jobj, 0x6, 0x400, AOBJ_StopAnim, 6, 0, 0);
+}
+// TODO: set in melee.link
+void *(*MOBJ_SetFlags)(MOBJ *mobj, u32 flags) = 0x80362D38;
+// TODO: set in melee.link
+void *(*MOBJ_ClearFlags)(MOBJ *mobj, u32 flags) = 0x80362D50;
 
 /**
  * Creates a Text and allows setting custom gx link and priority
@@ -35,5 +62,7 @@ Text* Text_CreateTextWithGX(int SisIndex, int canvasID, u8 gx_link, u8 gx_pri){
     stc_textcanvas_first[0]->gx_pri = 128;
     return text;
 }
+
+
 
 #endif SLIPPI_COMMON_H
