85f9fc8fda540e9db482cc88a7ac28499562def2
walz0 <aidan.m.walz@gmail.com>
2025-08-07 15:33:55 -0600

    add hover and select logic

--- Scenes/CSS/SheikSelector.c
+++ Scenes/CSS/SheikSelector.c
@@ -1,123 +1,164 @@
 #include "SheikSelector.h"
 
-typedef struct CSSPlayerData
-{
-    GOBJ *gobj;
-    u8 port;
-    u8 state;
-    u8 held_puck;
-    u8 unk;
-    short unk1;
-    short exit_timer;
-    Vec2 position;
+typedef struct CSSPlayerData {
+  GOBJ *gobj;
+  u8 port;
+  u8 state;
+  u8 held_puck;
+  u8 unk;
+  short unk1;
+  short exit_timer;
+  Vec2 position;
 } CSSPlayerData;
 
 int CSS_DATA_OFFSET = -0x49F0;
 
-bool poopbool = false;
-u8 GetPlayerIndex()
-{
-    /*
-        # Get location from which we can find selected character
-        lwz r4, -0x49F0(r13) # base address where css selections are stored
-        lbz r3, -0x49B0(r13) # player index
-        mulli r3, r3, 0x24
-        add REG_PORT_SELECTIONS_ADDR, r4, r3
-    */
-    const u8 PLAYER_INDEX_OFFSET = 0x40;
-    u8 playerIndex = *((u8 *)(R13_PTR(CSS_DATA_OFFSET + PLAYER_INDEX_OFFSET)));
-    return playerIndex;
+bool sheikSelected = true;
+bool zeldaSelected = false;
+
+u8 GetPlayerIndex() {
+  const u8 PLAYER_INDEX_OFFSET = 0x40;
+  u8 playerIndex = *((u8 *)(R13_PTR(CSS_DATA_OFFSET + PLAYER_INDEX_OFFSET)));
+  return playerIndex;
 }
 
-CSSData *GetPlayerSelections(u8 playerIndex)
-{
-    return (CSSData *)(R13_PTR(CSS_DATA_OFFSET + playerIndex * 0x24));
+CSSData *GetPlayerSelections(u8 playerIndex) {
+  return (CSSData *)(R13_PTR(CSS_DATA_OFFSET + playerIndex * 0x24));
 }
 
-void SetSelectedChar(u8 ckind)
-{
-    u8 playerIndex = GetPlayerIndex();
-    u8 *selectedChar = GetPlayerSelections(playerIndex)->data.data.players[playerIndex].c_kind;
+void SetSelectedChar(u8 ckind) {
+  u8 playerIndex = GetPlayerIndex();
+  u8 *selectedChar = GetPlayerSelections(playerIndex)->data.data.players[playerIndex].c_kind;
 
-    // Set character
-    selectedChar = ckind;
+  // Set character
+  selectedChar = ckind;
 
-    bp();
-    CSSIcon *iconData = 0x803f0cc8;
-    iconData->char_kind = ckind;
+  bp();
+  CSSIcon *iconData = 0x803f0cc8;
+  iconData->char_kind = ckind;
 
-    SFX_getCharacterNameAnnouncer(ckind);
+  SFX_getCharacterNameAnnouncer(ckind);
 
-    u8 port = GetPlayerIndex();
-    CSS_CursorHighlightUpdateCSPInfo(port);
+  u8 port = GetPlayerIndex();
+  CSS_CursorHighlightUpdateCSPInfo(port);
 }
 
-u8 GetSelectedChar()
-{
-    u8 selectedChar = *((u8 *)0x8043208f);
-    return selectedChar;
+u8 GetSelectedChar() {
+  u8 selectedChar = *((u8 *)0x8043208f);
+  return selectedChar;
 }
 
-Vec2 GetCursorPos()
-{
-    CSSPlayerData *cssPlayerData = *((unsigned int *)0x804A0BC0);
-    return cssPlayerData->position;
+Vec2 GetCursorPos() {
+  CSSPlayerData *cssPlayerData = *((unsigned int *)0x804A0BC0);
+  return cssPlayerData->position;
 }
 
-void InitSheikSelector()
-{
-    SlippiCSSDataTable *dt = GetSlpCSSDT();
-    SlpCSSDesc *slpCss = dt->SlpCSSDatAddress;
+void InitSheikSelector() {
+  SlippiCSSDataTable *dt = GetSlpCSSDT();
+  SlpCSSDesc *slpCss = dt->SlpCSSDatAddress;
 
-    // Initialize local player rank icon
-    GOBJ *gobj = GObj_Create(0x4, 0x5, 0x80);
-    selectorJobj = JOBJ_LoadJoint(slpCss->sheikSelector);
+  // Initialize local player rank icon
+  GOBJ *gobj = GObj_Create(0x4, 0x5, 0x80);
+  selectorJobj = JOBJ_LoadJoint(slpCss->sheikSelector);
 
-    JOBJ *zeldaIcon = selectorJobj->child;
-    JOBJ *sheikIcon = selectorJobj->child->sibling;
+  JOBJ *zeldaIcon = selectorJobj->child;
+  JOBJ *sheikIcon = selectorJobj->child->sibling;
 
-    JOBJ_SetAllAlpha(zeldaIcon, 0.5f);
-    // Set sheik to highlighted by default
-    JOBJ_SetAllAlpha(sheikIcon, 1.f);
+  JOBJ_SetAllAlpha(zeldaIcon, INACTIVE_ALPHA);
+  // Set sheik to highlighted by default
+  JOBJ_SetAllAlpha(sheikIcon, ACTIVE_ALPHA);
 
-    selectorJobj->trans.X = -8.f;
-    selectorJobj->trans.Y = -22.5f;
+  selectorJobj->trans.X = -8.f;
+  selectorJobj->trans.Y = -22.5f;
 
-    JOBJ_SetMtxDirtySub(selectorJobj);
+  JOBJ_SetMtxDirtySub(selectorJobj);
 
-    GObj_AddObject(gobj, 0x4, selectorJobj);
-    GObj_AddGXLink(gobj, GXLink_Common, 1, 129);
+  GObj_AddObject(gobj, 0x4, selectorJobj);
+  GObj_AddGXLink(gobj, GXLink_Common, 1, 129);
 }
 
-void UpdateSheikSelector()
-{
-    bool isLockedIn = SLIPPI_CSS_DATA_REF->dt->msrb->isLocalPlayerReady;
-    if (isLockedIn)
-    {
-        // Exit if player has locked their character selection
-        // TODO :: Does this hide the selector?
-        return;
-    }
+void UpdateSheikSelector() {
+  bool isLockedIn = SLIPPI_CSS_DATA_REF->dt->msrb->isLocalPlayerReady;
+  if (isLockedIn) {
+    // Exit if player has locked their character selection
+    // TODO :: Does this hide the selector?
+    return;
+  }
 
-    Vec2 cursorPos = GetCursorPos();
-    u8 selectedChar = GetSelectedChar(); // Get selected character
+  u8 port = GetPlayerIndex();
+  u64 downInputs = Pad_GetDown(port);
 
-    bool sheikSelected = selectedChar == CKIND_SHEIK;
-    bool zeldaSelected = selectedChar == CKIND_ZELDA;
-    if (sheikSelected || zeldaSelected)
-    {
-        if (selectorJobj)
-        {
-            JOBJ *zeldaIcon = selectorJobj->child;
-            JOBJ *sheikIcon = selectorJobj->child->sibling;
+  Vec2 cursorPos = GetCursorPos();
+  u8 selectedChar = GetSelectedChar();  // Get selected character
 
-            // First set alpha values based on who is currently selected
-            float zeldaAlpha = zeldaSelected ? 1.f : 0.5f;
-            JOBJ_SetAllAlpha(zeldaIcon, zeldaAlpha);
-            float sheikAlpha = sheikSelected ? 1.f : 0.5f;
-            JOBJ_SetAllAlpha(sheikIcon, sheikAlpha);
+  if (selectedChar == CKIND_SHEIK || selectedChar == CKIND_ZELDA) {
+    if (selectorJobj) {
+      // Show selector UI
+      JOBJ_SetAllAlpha(selectorJobj, 1.f);
+
+      JOBJ *zeldaIcon = selectorJobj->child;
+      JOBJ *sheikIcon = selectorJobj->child->sibling;
+
+      bool sheikHovered = false;
+      bool zeldaHovered = false;
+
+      JOBJ *buttons[2] = {zeldaIcon, sheikIcon};
+      for (int i = 0; i < 2; i++) {
+        bool isSheik = i == 1;
+        const float BUTTON_BOTTOM = BUTTON_TOP - BUTTON_HEIGHT;
+        // Check if cursor is at the height of the selector
+        if (cursorPos.Y < BUTTON_TOP && cursorPos.Y >= BUTTON_BOTTOM) {
+          float buttonLeft = BUTTON_CONTAINER_START_X + i * BUTTON_WIDTH;
+          float buttonRight = buttonLeft + BUTTON_WIDTH;
+          // Check if cursor is within the bounds of this button
+          if (cursorPos.X > buttonLeft && cursorPos.X < buttonRight) {
+            sheikHovered = isSheik;
+            zeldaHovered = !isSheik;
+
+            // JOBJ_SetAllAlpha(buttons[i], HOVER_ALPHA);
+            if (downInputs & HSD_BUTTON_A) {
+              // Change character
+              if (isSheik) {
+                // OSReport("Selecting Sheik\n");
+                SetSelectedChar(CKIND_SHEIK);
+              } else {
+                // OSReport("Selecting Zelda\n");
+                SetSelectedChar(CKIND_ZELDA);
+              }
+              sheikSelected = isSheik;
+              zeldaSelected = !isSheik;
+            }
+          }
         }
+      }
 
-        // TODO :: Highlight hovered icon
+      float zeldaAlpha = INACTIVE_ALPHA;
+      float sheikAlpha = INACTIVE_ALPHA;
+
+      if (zeldaHovered) {
+        zeldaAlpha = HOVER_ALPHA;
+      }
+      if (sheikHovered) {
+        sheikAlpha = HOVER_ALPHA;
+      }
+
+      if (zeldaSelected) {
+        zeldaAlpha = ACTIVE_ALPHA;
+      }
+      if (sheikSelected) {
+        sheikAlpha = ACTIVE_ALPHA;
+      }
+
+      //   OSReport("zeldaHovered: %d\n", zeldaHovered);
+      //   OSReport("sheikHovered: %d\n", sheikHovered);
+      //   OSReport("zeldaSelected: %d\n", zeldaSelected);
+      //   OSReport("sheikSelected: %d\n", sheikSelected);
+
+      JOBJ_SetAllAlpha(zeldaIcon, zeldaAlpha);
+      JOBJ_SetAllAlpha(sheikIcon, sheikAlpha);
     }
+  } else {
+    // Hide selector UI
+    JOBJ_SetAllAlpha(selectorJobj, 0.f);
+  }
 }
--- Scenes/CSS/SheikSelector.h
+++ Scenes/CSS/SheikSelector.h
@@ -1,177 +1,180 @@
 #include "../../Slippi.h"
 
-typedef struct CSSIcon
-{
-    u8 ft_hudindex; // 0x00 - used for getting combo count @ 8025C0C4
-    u8 char_kind;   // 0x01 - icons external ID
-    u8 state;       // 0x02 - 0x0 Not Unlocked, 0x01 Unlocked, 0x02 Unlocked and disp
-    u8 anim_timer;  // 0x03 - 0xC when char is chosen
-    u8 joint_id_vs; // 0x04 - Vs Icon Bg Jobj ID
-    u8 joint_id_1p; // 0x05 - Vs Icon Bg Jobj ID
-    int sfx;        // 0x08
-    float bound_l;  // 0x0C
-    float bound_r;  // 0x10
-    float bound_u;  // 0x14
-    float bound_d;  // 0x18
+typedef struct CSSIcon {
+  u8 ft_hudindex;  // 0x00 - used for getting combo count @ 8025C0C4
+  u8 char_kind;    // 0x01 - icons external ID
+  u8 state;        // 0x02 - 0x0 Not Unlocked, 0x01 Unlocked, 0x02 Unlocked and disp
+  u8 anim_timer;   // 0x03 - 0xC when char is chosen
+  u8 joint_id_vs;  // 0x04 - Vs Icon Bg Jobj ID
+  u8 joint_id_1p;  // 0x05 - Vs Icon Bg Jobj ID
+  int sfx;         // 0x08
+  float bound_l;   // 0x0C
+  float bound_r;   // 0x10
+  float bound_u;   // 0x14
+  float bound_d;   // 0x18
 } CSSIcon;
 
-typedef struct PlayerInitData
-{
-    s8 c_kind;    // character kind
-    u8 slot_type; // slot type (0 = human, 1 = cpu?)
-    s8 stocks;    // stocks
-    u8 color;     // color
-    u8 slot;      // port
-    s8 x5;        // spawnpos32
-    s8 spawn_dir; // spawn direction
-    u8 sub_color; // subcolor
-    s8 handicap;  // handicap
-    u8 team;      // team
-    s8 xA;        // nametag
-    u8 xB;
-    u8 xC_b0 : 1;
-    u8 xC_b1 : 1;
-    u8 xC_b2 : 1;
-    u8 xC_b3 : 1;
-    u8 xC_b4 : 1;
-    u8 xC_b5 : 1;
-    u8 xC_b6 : 1;
-    u8 xC_b7 : 1;
-    u8 xD_b0 : 1;
-    u8 xD_b1 : 1;
-    u8 xD_b2 : 1;
-    u8 xD_b3 : 1;
-    u8 xD_b4 : 1;
-    u8 xD_b5 : 1;
-    u8 xD_b6 : 1;
-    u8 xD_b7 : 1;
-    u8 xE;
-    u8 cpu_level; // CPU level
-    u16 x10;
-    u16 x12;
-    u16 x14;
-    float x18; // offense ratio
-    float x1C; // defense ratio
-    float x20;
+typedef struct PlayerInitData {
+  s8 c_kind;     // character kind
+  u8 slot_type;  // slot type (0 = human, 1 = cpu?)
+  s8 stocks;     // stocks
+  u8 color;      // color
+  u8 slot;       // port
+  s8 x5;         // spawnpos32
+  s8 spawn_dir;  // spawn direction
+  u8 sub_color;  // subcolor
+  s8 handicap;   // handicap
+  u8 team;       // team
+  s8 xA;         // nametag
+  u8 xB;
+  u8 xC_b0 : 1;
+  u8 xC_b1 : 1;
+  u8 xC_b2 : 1;
+  u8 xC_b3 : 1;
+  u8 xC_b4 : 1;
+  u8 xC_b5 : 1;
+  u8 xC_b6 : 1;
+  u8 xC_b7 : 1;
+  u8 xD_b0 : 1;
+  u8 xD_b1 : 1;
+  u8 xD_b2 : 1;
+  u8 xD_b3 : 1;
+  u8 xD_b4 : 1;
+  u8 xD_b5 : 1;
+  u8 xD_b6 : 1;
+  u8 xD_b7 : 1;
+  u8 xE;
+  u8 cpu_level;  // CPU level
+  u16 x10;
+  u16 x12;
+  u16 x14;
+  float x18;  // offense ratio
+  float x1C;  // defense ratio
+  float x20;
 } PlayerInitData;
-typedef struct StartMeleeRules
-{
-    u32 x0_0 : 3; // match mode? 1 = stock mode, 2 = coin mode?
-    u32 x0_3 : 3;
-    u32 x0_6 : 1;
-    u32 x0_7 : 1;
+typedef struct StartMeleeRules {
+  u32 x0_0 : 3;  // match mode? 1 = stock mode, 2 = coin mode?
+  u32 x0_3 : 3;
+  u32 x0_6 : 1;
+  u32 x0_7 : 1;
 
-    u32 x1_0 : 1;
-    u32 x1_1 : 1;
-    u32 x1_2 : 1;
-    u32 x1_3 : 1;
-    u32 x1_4 : 1;
-    u32 x1_5 : 1;
-    u32 timer_shows_hours : 1; // false=65:00.00, true=1:05:00.00
-    u32 x1_7 : 1;
+  u32 x1_0 : 1;
+  u32 x1_1 : 1;
+  u32 x1_2 : 1;
+  u32 x1_3 : 1;
+  u32 x1_4 : 1;
+  u32 x1_5 : 1;
+  u32 timer_shows_hours : 1;  // false=65:00.00, true=1:05:00.00
+  u32 x1_7 : 1;
 
-    u32 x2_0 : 1;
-    u32 x2_1 : 1;
-    u32 x2_2 : 1;
-    u32 x2_3 : 1;
-    u32 x2_4 : 1;
-    u32 x2_5 : 1;
-    u32 x2_6 : 1;
-    u32 x2_7 : 1;
+  u32 x2_0 : 1;
+  u32 x2_1 : 1;
+  u32 x2_2 : 1;
+  u32 x2_3 : 1;
+  u32 x2_4 : 1;
+  u32 x2_5 : 1;
+  u32 x2_6 : 1;
+  u32 x2_7 : 1;
 
-    u32 x3_0 : 1;
-    u32 x3_1 : 1;
-    u32 x3_2 : 1;
-    u32 x3_3 : 1;
-    u32 x3_4 : 1;
-    u32 x3_5 : 1;
-    u32 x3_6 : 1;
-    u32 x3_7 : 1;
+  u32 x3_0 : 1;
+  u32 x3_1 : 1;
+  u32 x3_2 : 1;
+  u32 x3_3 : 1;
+  u32 x3_4 : 1;
+  u32 x3_5 : 1;
+  u32 x3_6 : 1;
+  u32 x3_7 : 1;
 
-    u32 x4_0 : 1;
-    u32 x4_1 : 1;
-    u32 x4_2 : 1;
-    u32 x4_3 : 1;
-    u32 x4_4 : 1;
-    u32 x4_5 : 1;
-    u32 x4_6 : 1;
-    u32 x4_7 : 1;
+  u32 x4_0 : 1;
+  u32 x4_1 : 1;
+  u32 x4_2 : 1;
+  u32 x4_3 : 1;
+  u32 x4_4 : 1;
+  u32 x4_5 : 1;
+  u32 x4_6 : 1;
+  u32 x4_7 : 1;
 
-    u32 x5_0 : 1;
-    u32 x5_1 : 1;
-    u32 x5_2 : 1;
-    u32 x5_3 : 1;
-    u32 x5_4 : 1;
-    u32 x5_5 : 1;
-    u32 x5_6 : 1;
-    u32 x5_7 : 1;
+  u32 x5_0 : 1;
+  u32 x5_1 : 1;
+  u32 x5_2 : 1;
+  u32 x5_3 : 1;
+  u32 x5_4 : 1;
+  u32 x5_5 : 1;
+  u32 x5_6 : 1;
+  u32 x5_7 : 1;
 
-    u8 x6;
-    u8 x7;
-    u8 x8; // is teams
-    u8 x9;
-    u8 xA;
-    s8 xB; // item frequency
-    s8 xC; // SD penalty
-    u8 xD;
-    u16 xE; // InternalStageId
+  u8 x6;
+  u8 x7;
+  u8 x8;  // is teams
+  u8 x9;
+  u8 xA;
+  s8 xB;  // item frequency
+  s8 xC;  // SD penalty
+  u8 xD;
+  u16 xE;  // InternalStageId
 
-    u32 x10; // time limit
-    u8 x14;
-    u32 x18;
-    u32 x1C_pad[(0x20 - 0x1C) / 4];
+  u32 x10;  // time limit
+  u8 x14;
+  u32 x18;
+  u32 x1C_pad[(0x20 - 0x1C) / 4];
 
-    u64 x20; // item mask
-    int x28;
-    float x2C;
-    float x30; // damage ratio
-    float x34; // game speed
-    void (*x38)(int);
-    void (*x3C)(int);
-    int (*x40)(void);
-    void (*x44)(void);
-    void (*x48)(void);
-    void (*x4C)(void);
-    void (*x50)(int);
-    struct
-    {
-        u8 pad_x0[0x10];
-        u8 x10_b0 : 1;
-        u8 x10_b1 : 1;
-    } *x54;
-    int x58;
-    u8 pad_x5C[0x60 - 0x5C];
+  u64 x20;  // item mask
+  int x28;
+  float x2C;
+  float x30;  // damage ratio
+  float x34;  // game speed
+  void (*x38)(int);
+  void (*x3C)(int);
+  int (*x40)(void);
+  void (*x44)(void);
+  void (*x48)(void);
+  void (*x4C)(void);
+  void (*x50)(int);
+  struct
+  {
+    u8 pad_x0[0x10];
+    u8 x10_b0 : 1;
+    u8 x10_b1 : 1;
+  } *x54;
+  int x58;
+  u8 pad_x5C[0x60 - 0x5C];
 } StartMeleeRules;
 
-typedef struct StartMeleeData
-{
-    StartMeleeRules rules;
-    PlayerInitData players[6];
+typedef struct StartMeleeData {
+  StartMeleeRules rules;
+  PlayerInitData players[6];
 } StartMeleeData;
 
-typedef struct VsModeData
-{
-    s8 loser;
-    s8 ordered_stage_index;
-    s8 winner;
-    u8 unk_0x3;
-    u8 unk_0x4;
-    u8 unk_0x5;
-    u8 unk_0x6;
-    u8 unk_0x7;
-    StartMeleeData data;
+typedef struct VsModeData {
+  s8 loser;
+  s8 ordered_stage_index;
+  s8 winner;
+  u8 unk_0x3;
+  u8 unk_0x4;
+  u8 unk_0x5;
+  u8 unk_0x6;
+  u8 unk_0x7;
+  StartMeleeData data;
 } VsModeData;
 
-typedef struct CSSData
-{
-    u16 unk_0x0; ///< 1p port?
-    u8 match_type;
-    u8 pending_scene_change;
-    u8 *ko_star_counts;
-    VsModeData data;
+typedef struct CSSData {
+  u16 unk_0x0;  ///< 1p port?
+  u8 match_type;
+  u8 pending_scene_change;
+  u8 *ko_star_counts;
+  VsModeData data;
 } CSSData;
 
 JOBJ *selectorJobj;
+
+const float BUTTON_TOP = -20.f;
+const float BUTTON_CONTAINER_START_X = -15.75f;
+const float BUTTON_HEIGHT = 3.f;
+const float BUTTON_WIDTH = 3.25f;
+
+const float ACTIVE_ALPHA = 1.0f;
+const float INACTIVE_ALPHA = 0.4f;
+const float HOVER_ALPHA = 0.75f;
 
 void UpdateSheikSelector();
 
